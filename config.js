1a2ee8952d57acbf38c2aa0d7d3ce1b5d68aef9e
Dead Boy <87977420+DEVILX1100@users.noreply.github.com>
2022-05-18 18:19:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@ module.exports = {
     Developers: ["491577179495333903", "767393101241122826"],
 
     Bot: {
-        ClientID: "880372206163623966",
-        ClientSecret: "XXXXXXXXXXXXXXXXXXXX",
+        ClientID: "964027855187087380",
+        ClientSecret: "_xpdGUNQv0cCvM0PW9WRd2KK6PCY5ET9",
         ClientToken: process.env.TOKEN, // Bot Token here
         Color: "BLURPLE", 
         BotPrefix: "!" // Prefix
     },
 
 
-    Mongoose: "mongodb+srv://Xxxxxxxxxxxxxxxxx", // MongoDb Url
+    Mongoose: "mongodb+srv://Radio-Music:radiomusic123@cluster0.gv1x8.mongodb.net/Radio-Music", // MongoDb Url
 
 }
